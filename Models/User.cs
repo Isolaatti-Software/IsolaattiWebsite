f058f6046b8fe21611b62c7c47daa057d2f5f9cd
erik <erik10cavazos@gmail.com>
2020-04-12 02:33:31 -0500

    make some corrections to names

--- Models/User.cs
+++ Models/User.cs
@@ -7,7 +7,16 @@ namespace isolaatti_API.Models
         public string Email { get; set; }
         public string Password { get; set; }
         public string Uid { get; set; }
-        public bool emailValidated { get; set; }
-        public string googleToken { get; set; }
+        public bool EmailValidated { get; set; }
+        public string GoogleToken { get; set; }
+        
+        
+        // fields for user preferences
+        public bool NotifyByEmail { get; set; }
+        public bool NotifyWhenProcessStarted { get; set; }
+        public bool NotifyWhenProcessFinishes { get; set; }
+        
+        // language field is used to decide what language to use in notifications by E-mail
+        public string AppLanguage { get; set; }
     }
 }
