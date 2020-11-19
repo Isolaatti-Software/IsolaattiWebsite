76a46cf9159c198481fd35e7d3a2e9cc99286105
erik <erik10cavazos@gmail.com>
2020-11-19 01:00:55 -0600

    page for shared public links

---
+++ Pages/publicAPI/Shared.cshtml.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Linq;
+using isolaatti_API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace isolaatti_API.Pages.publicAPI
+{
+    public class Shared : PageModel
+    {
+        private readonly DbContextApp db;
+        public Shared(DbContextApp dbContextApp)
+        {
+            db = dbContextApp;
+        }
+        public IActionResult OnGet(string uid)
+        {
+            try
+            {
+                var songRef = db.SharedSongs.Single(song => song.uid.Equals(uid));
+                var song = db.Songs.Find(songRef.SharedSongId);
+                var owner = db.Users.Find(song.OwnerId);
+                ViewData["name"] = song.OriginalFileName;
+                ViewData["artist"] = song.Artist;
+                ViewData["owner"] = owner.Name;
+                ViewData["bass_url"] = song.BassUrl;
+                ViewData["drums_url"] = song.DrumsUrl;
+                ViewData["vocals_url"] = song.VoiceUrl;
+                ViewData["other_url"] = song.OtherUrl;
+                return Page();
+            }
+            catch (Exception e)
+            {
+                return null;
+            }
+            
+        }
+    }
+}
