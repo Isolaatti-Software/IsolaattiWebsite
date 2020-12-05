238e3654522c88ca5200b19bd76b788ef6528705
erik <erik10cavazos@gmail.com>
2020-12-04 19:43:58 -0600

    remove unneeded httpClient

--- Controllers/DeleteSongController.cs
+++ Controllers/DeleteSongController.cs
@@ -1,9 +1,5 @@
-﻿using System.Collections.Generic;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using isolaatti_API.Models;
-using System.Net.Http;
-using System.Net.Http.Formatting;
-using Flurl.Http;
 
 namespace isolaatti_API.Controllers
 {
@@ -12,13 +8,6 @@ namespace isolaatti_API.Controllers
     public class DeleteSongController : ControllerBase
     {
         private readonly DbContextApp db;
-        private static readonly HttpClient httpClient;
-
-        static DeleteSongController()
-        {
-            httpClient = new HttpClient();
-        }
-
         public DeleteSongController(DbContextApp contextApp)
         {
             db = contextApp;
