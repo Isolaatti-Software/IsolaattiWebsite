ae6f52d6c62d8d978e840c97d119c9d22ec1c847
erik <erik10cavazos@gmail.com>
2020-12-19 00:33:47 -0600

    code to upload songs

---
+++ wwwroot/js/uploader.js
@@ -0,0 +1,67 @@
+var storageRef = storage.ref();
+
+let fileSource = document.getElementById("audioFileInput");
+let buttonUploadFile = document.getElementById("btnUploadAudioFile");
+let fieldSongName = document.getElementById("field_songs_name");
+let fieldArtistName = document.getElementById("field_songs_artist");
+let progressBarContainer = document.getElementById("progress-bar-container");
+let progressBar = document.getElementById("file_upload_progress_bar");
+
+fileSource.addEventListener("change", function(){
+    fieldSongName.disabled = false;
+    fieldArtistName.disabled = false;
+    fieldSongName.value = fileSource.files[0].name; 
+});
+
+buttonUploadFile.addEventListener("click",function (){
+    if(fieldSongName.value === "" || fieldArtistName.value === "")
+        alert("Field cannot be empty");
+    else{
+        buttonUploadFile.disabled = true;
+        fileSource.disabled = true;
+        fieldArtistName.disabled = true;
+        fieldSongName.disabled = true;
+        progressBarContainer.style.visibility = "visible";
+        let uploadTask = storageRef.child(`source_files/${fileSource.files[0].name}`).put(fileSource.files[0]);
+        
+        let songName = fieldSongName.value;
+        let songArtist = fieldArtistName.value;
+
+        uploadTask.on("state_changed",function(snapshot){
+            //updates progress bar
+            progressBar.style.width = `${(snapshot.bytesTransferred / snapshot.totalBytes) * 100}%`;
+        }, function(error){
+            alert(error)
+        }, function(){
+            buttonUploadFile.disabled = false;
+            fileSource.disabled = false;
+            fieldArtistName.disabled = false;
+            fieldSongName.disabled = false;
+            uploadTask.snapshot.ref.getDownloadURL().then(function(downloadURL) {
+                console.log('File available at', downloadURL);
+                registerFile(downloadURL,songName,songArtist);
+            });
+        });
+    }
+});
+
+function registerFile(fileUrl, songName, songArtist){
+    let formData = new FormData();
+    formData.append("userId",userData.id);
+    formData.append("songName", songName);
+    formData.append("url", fileUrl);
+    
+    if(songArtist !== "")
+        formData.append("songArtist",songArtist);
+    
+    let createRecordRequest = new XMLHttpRequest();
+    createRecordRequest.open("POST", "/AddSongToQueue");
+    createRecordRequest.send(formData);
+    createRecordRequest.onreadystatechange = function (){
+        if(createRecordRequest.readyState === XMLHttpRequest.DONE){
+            alert("Song added to queue");
+            window.location = "/WebApp/OnQueue";
+        }
+    }
+        
+}
