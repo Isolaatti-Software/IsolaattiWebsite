7ec2e0cb83b0518f303da3ea68c1e8463fd02844
erik <erik10cavazos@gmail.com>
2020-12-19 09:09:14 -0600

    now when main endpoint is requested user is redirected to webapp

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,12 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace isolaatti_API.Pages
 {
     public class Index : PageModel
     {
-        public void OnGet()
+        public IActionResult OnGet()
         {
-            
+            return RedirectToPage("/WebApp/Index");
         }
     }
 }
