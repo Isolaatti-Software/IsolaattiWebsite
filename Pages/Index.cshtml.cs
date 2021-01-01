a2954130d98a434432bf69d5d4cb675ebb2588bb
erik <erik10cavazos@gmail.com>
2021-01-01 10:42:17 -0600

    Now Index redirects to webapp if already signed in

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -4,6 +4,8 @@
 * This program is not allowed to be copied or reused without explicit permission.
 * erik10cavazos@gmail.com and everardo.cavazoshrnnd@uanl.edu.mx
 */
+
+using isolaatti_API.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -11,8 +13,18 @@ namespace isolaatti_API.Pages
 {
     public class Index : PageModel
     {
+        private readonly DbContextApp db;
+
+        public Index(DbContextApp dbContextApp)
+        {
+            db = dbContextApp;
+        }
         public IActionResult OnGet()
         {
+            if (Request.Cookies["isolaatti_user_name"] != null && Request.Cookies["isolaatti_user_password"] != null)
+            {
+                return RedirectToPage("/WebApp/Index");
+            }
             return Page();
         }
     }
