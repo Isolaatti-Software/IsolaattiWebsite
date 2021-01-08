de0433a95a63c3a0db8ece279ee87732340e0a72
erik <erik10cavazos@gmail.com>
2021-01-08 01:32:23 -0600

    added "All" action to UnshareSong controller. Some security error fixed too.

--- Controllers/UnshareSong.cs
+++ Controllers/UnshareSong.cs
@@ -10,21 +10,39 @@ using isolaatti_API.Models;
 
 namespace isolaatti_API.Controllers
 {
-    public class UnshareSong : Controller
+    [ApiController]
+    [Route("[controller]")]
+    public class UnshareSong : ControllerBase
     {
         private readonly DbContextApp db;
-
         public UnshareSong(DbContextApp _db)
         {
             db = _db;
         }
         [HttpPost]
-        public void Index([FromForm] int userId, [FromForm] string pwd, [FromForm] int songId)
+        public IActionResult Index([FromForm] int userId, [FromForm] string pwd, [FromForm] int songId)
         {
-            db.SharedSongs.Remove(
-                db.SharedSongs.Single(s => s.SharedSongId.Equals(songId))
-                );
+            var user = db.Users.Find(userId);
+            if (user == null) return NotFound("Unknown user");
+            if (!user.Password.Equals(pwd)) return Unauthorized("Wrong password");
+            var elementToUnshare = db.SharedSongs.Single(s => s.SharedSongId.Equals(songId));
+            if (!elementToUnshare.userId.Equals(user.Id)) return Unauthorized("This share is not yours!");
+            db.SharedSongs.Remove(elementToUnshare);
             db.SaveChanges();
+            return Ok("Unshared successfully");
+        }
+
+        [HttpPost]
+        [Route("All")]
+        public IActionResult DeleteAll([FromForm] int userId, [FromForm] string password)
+        {
+            var user = db.Users.Find(userId);
+            if (user == null) return NotFound("Unknown user");
+            if (!user.Password.Equals(password)) return Unauthorized("Wrong password");
+            var allShares = db.SharedSongs.Where(element => element.userId.Equals(user.Id));
+            db.SharedSongs.RemoveRange(allShares);
+            db.SaveChanges();
+            return Ok("All shares were deleted successfully");
         }
     }
 }
