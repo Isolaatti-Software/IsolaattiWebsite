d00a274ea1aeab40d22c0a15913bdabb1d93ef7f
Erik <erik10cavazos@gmail.com>
2021-01-08 23:12:50 -0600

    really simple notifications manager (improve this)

---
+++ Pages/WebApp/Notifications.cshtml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+using isolaatti_API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace isolaatti_API.Pages.WebApp
+{
+    public class Notifications : PageModel
+    {
+        private readonly DbContextApp db;
+        public Notifications(DbContextApp dbContext)
+        {
+            db = dbContext;
+        }
+
+        public IQueryable<Notification> UserNotifications;
+        public IActionResult OnGet()
+        {
+            var email = Request.Cookies["isolaatti_user_name"];
+            var password = Request.Cookies["isolaatti_user_password"];
+
+            if (email == null || password == null)
+            {
+                return RedirectToPage("LogIn");
+            }
+
+            try
+            {
+                var user = db.Users.Single(user => user.Email.Equals(email));
+                if (user.Password.Equals(password))
+                {
+                    if (!user.EmailValidated)
+                        return RedirectToPage("LogIn", new
+                        {
+                            username = email,
+                            notVerified = true
+                        });
+                    // here it's know that account is correct. Data binding!
+                    ViewData["name"] = user.Name;
+                    ViewData["email"] = user.Email;
+                    ViewData["userId"] = user.Id;
+                    ViewData["password"] = user.Password;
+                    
+                    // Get user notifications
+                    UserNotifications = db.Notifications.Where(notification => notification.UserId.Equals(user.Id))
+                        .OrderByDescending(notification => notification.Id);
+                    return Page();
+                }
+            }
+            catch (InvalidOperationException)
+            {
+                return RedirectToPage("LogIn");
+            }
+            
+            return RedirectToPage("LogIn", new
+            {
+                badCredential = true,
+                username = email
+            });
+        }
+    }
+}
