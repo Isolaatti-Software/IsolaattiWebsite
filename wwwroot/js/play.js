a4ec92f3bffeab836d6b54d887685dfff488598a
Erik <erik10cavazos@gmail.com>
2021-01-14 23:19:51 -0600

    fully working: main gain, tracks gain, panning and seeking bar

--- wwwroot/js/play.js
+++ wwwroot/js/play.js
@@ -8,6 +8,7 @@
 */
 let mediaElements = document.querySelectorAll(".track-audio-element");
 let seekPositionSlider = document.querySelector("#seek-position-slider");
+let playButton = document.querySelector("#play_pause_button");
 let isolaattiMixer = new IsolaattiAudioMixer(mediaElements, function(event){
     /* Handle here slider of current position */
     seekPositionSlider.value = event.target.currentTime;
@@ -19,21 +20,49 @@ isolaattiMixer.prepareMix(function(){
     defineEvents();
 });
 
+isolaattiMixer.setOnMixEnded(function(event){
+    playButton.innerHTML = '<i class="fas fa-play"></i>';
+    seekPositionSlider.value = 0;
+});
+
 function defineEvents() {
     // set events here
-    let playButton = document.querySelector("#play_pause_button");
     playButton.addEventListener("click", function() {
         if(isolaattiMixer.playing){
             isolaattiMixer.pauseMix();
+            playButton.innerHTML = '<i class="fas fa-play"></i>';
         } else {
             isolaattiMixer.playMix();
+            playButton.innerHTML = '<i class="fas fa-pause"></i>';
         }
     });
 
-    
+    // seeking slider
     seekPositionSlider.max = isolaattiMixer.getDuration();
-    seekPositionSlider.addEventListener("input", function(event){
+    seekPositionSlider.addEventListener("input", function(event) {
        isolaattiMixer.seekTo(event.target.value);
+    });
+    
+    // main gain slider
+    let mainGainSlider = document.querySelector("#mix_gain");
+    mainGainSlider.addEventListener("input", function() {
+        isolaattiMixer.setMainGainValue(mainGainSlider.value);
+    });
+    
+    // gains of every track
+    let tracksGainSliders = document.querySelectorAll(".track-gain");
+    tracksGainSliders.forEach(function(slider) {
+        slider.addEventListener("input",function() {
+            isolaattiMixer.setGainOfTrack(slider.attributes.getNamedItem("target-track").value, slider.value);
+        });
+    });
+    
+    // stereo panner of every track
+    let stereoPanningSliders = document.querySelectorAll(".balance_slider");
+    stereoPanningSliders.forEach(function(slider) {
+       slider.addEventListener("input", function() {
+           isolaattiMixer.setPannerValueOfTrack(slider.attributes.getNamedItem("target-track").value, slider.value);
+       });
     });
 }
 
