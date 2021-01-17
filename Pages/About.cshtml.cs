f8efebf74ddf8461ec59d547d620f43dce0e4444
erik <erik10cavazos@gmail.com>
2021-01-16 19:29:00 -0600

    some design rework

---
+++ Pages/About.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace isolaatti_API.Pages
+{
+    public class About : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
