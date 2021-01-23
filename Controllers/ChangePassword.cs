cdbf24b2bbd50143ae4d766c0cbb35882d0c5de3
erik <erik10cavazos@gmail.com>
2021-01-23 00:03:12 -0600

    Changed Route for every api controller. /api/{controller}

--- Controllers/ChangePassword.cs
+++ Controllers/ChangePassword.cs
@@ -10,7 +10,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace isolaatti_API.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class ChangePassword : ControllerBase
     {
         private readonly DbContextApp Db;
