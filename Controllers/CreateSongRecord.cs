6a481eedaf6a6302c43a33b9f8d5b16ed70e5193
erik <erik10cavazos@gmail.com>
2021-03-06 17:24:46 -0600

    create default TracksSettings json array for new songs

--- Controllers/CreateSongRecord.cs
+++ Controllers/CreateSongRecord.cs
@@ -4,6 +4,10 @@
 * This program is not allowed to be copied or reused without explicit permission.
 * erik10cavazos@gmail.com and everardo.cavazoshrnnd@uanl.edu.mx
 */
+
+using System.Collections.Generic;
+using System.Text.Json;
+using isolaatti_API.Classes;
 using isolaatti_API.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -22,11 +26,42 @@ namespace isolaatti_API.Controllers
         [HttpPost]
         public int Index([FromForm]int userId, [FromForm]string fileName, [FromForm]string songArtist="Unknown")
         {
+            var listOfPreferences = new List<TrackPreferences>();
+            listOfPreferences.Add(new TrackPreferences()
+            {
+                Name = "Bass",
+                GainValue = 1.0f,
+                HtmlColorHex = "#000000",
+                PanningValue = 0
+            });
+            listOfPreferences.Add(new TrackPreferences()
+            {
+                Name = "Drums",
+                GainValue = 1.0f,
+                HtmlColorHex = "#000000",
+                PanningValue = 0
+            });
+            listOfPreferences.Add(new TrackPreferences()
+            {
+                Name = "Vocals",
+                GainValue = 1.0f,
+                HtmlColorHex = "#000000",
+                PanningValue = 0
+            });
+            listOfPreferences.Add(new TrackPreferences()
+            {
+                Name = "Other",
+                GainValue = 1.0f,
+                HtmlColorHex = "#000000",
+                PanningValue = 0
+            });
+            
             Song songToAdd = new Song()
             {
                 OwnerId = userId,
                 OriginalFileName = fileName,
-                Artist = songArtist
+                Artist = songArtist,
+                TracksSettings = JsonSerializer.Serialize(listOfPreferences)
             };
             _contextApp.Songs.Add(songToAdd);
             _contextApp.SaveChanges();
