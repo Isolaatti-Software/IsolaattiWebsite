4bc381f18c35da69c61eb2bd266b7ceca9037258
erik <erik10cavazos@gmail.com>
2021-03-23 21:45:50 -0600

    added field to Likes table: TargetUserId

--- Models/Like.cs
+++ Models/Like.cs
@@ -5,5 +5,6 @@ namespace isolaatti_API.Models
         public int Id { get; set; }
         public long PostId { get; set; }
         public int UserId { get; set; }
+        public int TargetUserId { get; set; }
     }
 }
