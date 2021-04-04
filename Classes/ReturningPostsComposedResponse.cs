5c701a98f945d78b80af2905a3d253afc49b7e31
erik <erik10cavazos@gmail.com>
2021-04-03 18:29:39 -0600

    added UserName property to post composed response

--- Classes/ReturningPostsComposedResponse.cs
+++ Classes/ReturningPostsComposedResponse.cs
@@ -5,6 +5,7 @@ namespace isolaatti_API.Classes
         public long Id { get; set; }
         public string TextContent { get; set; }
         public int UserId { get; set; }
+        public string UserName { get; set; }
         public long NumberOfLikes { get; set; }
         public int Privacy { get; set; }
         public bool Liked { get; set; }
