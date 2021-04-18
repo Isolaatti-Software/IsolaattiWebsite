b5f28871cd676abf0ab62840c666a646f0f9411f
erik <erik10cavazos@gmail.com>
2021-04-17 21:32:23 -0500

    fixed error in SessionToken.cs: no Token was initialized

--- Models/SessionToken.cs
+++ Models/SessionToken.cs
@@ -5,7 +5,12 @@ namespace isolaatti_API.Models
     public class SessionToken
     {
         public int Id { get; set; }
-        public string Token = Guid.NewGuid().ToString();
+        public string Token { get; set; }
         public int UserId { get; set; }
+
+        public SessionToken()
+        {
+            Token = Guid.NewGuid().ToString();
+        }
     }
 }
