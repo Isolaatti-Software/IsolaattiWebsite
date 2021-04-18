64e3952048a420680ddeac29675fc0e0b4480d38
erik <erik10cavazos@gmail.com>
2021-04-18 16:24:38 -0500

    feature added: when a post is deleted, all its comments and likes are removed too

--- Controllers/EditPost.cs
+++ Controllers/EditPost.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using isolaatti_API.isolaatti_lib;
 using isolaatti_API.Models;
 using Microsoft.AspNetCore.Mvc;
@@ -48,6 +49,12 @@ namespace isolaatti_API.Controllers
             
             // Yep, here I can delete the post
             Db.SimpleTextPosts.Remove(post);
+            var commentsOfPost = Db.Comments.Where(comment => comment.SimpleTextPostId == post.Id).ToList();
+            Db.Comments.RemoveRange(commentsOfPost);
+
+            var likesOfPost = Db.Likes.Where(like => like.PostId == post.Id).ToList();
+            Db.Likes.RemoveRange(likesOfPost);
+            
             Db.SaveChanges();
             
             return Ok("Post deleted");
