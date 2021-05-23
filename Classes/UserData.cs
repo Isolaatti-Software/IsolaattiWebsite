82db4ce95d47fcff3652000a178948e5bc4cd3d0
erik <erik10cavazos@gmail.com>
2021-05-23 02:31:49 -0500

    simplified UserData class

--- Classes/UserData.cs
+++ Classes/UserData.cs
@@ -11,34 +11,9 @@ namespace isolaatti_API.Classes
 {
     public class UserData
     {
-        private readonly DbContextApp dbContext;
-        private User user;
-        public string name { get; }
-        public string profilePhotoUrl { get; }
-        public string emailAddress { get; }
-        public Boolean badPassword { get; }
-        public Boolean validatedEmail { get; }
-        public int userId { get; }
-        public UserData(int userId, DbContextApp dbContextApp)
-        {
-            dbContext = dbContextApp;
-            user = dbContext.Users.Find(userId);
-            name = user.Name;
-            emailAddress = user.Email;
-            profilePhotoUrl = ""; //fill this when implemented
-            validatedEmail = user.EmailValidated;
-            this.userId = userId;
-        }
-        public UserData(Boolean badPass)
-        {
-            if (badPass)
-            {
-                badPassword = true;
-            }
-            else
-            {
-                badPassword = false;
-            }
-        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ProfilePhotoUrl { get; set; }
+        public string Email { get; set; }
     }
 }
