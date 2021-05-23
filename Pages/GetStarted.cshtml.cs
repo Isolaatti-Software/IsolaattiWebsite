c1866ce129af8e24c5ecd2230ad70d6e0598b223
erik <erik10cavazos@gmail.com>
2021-05-23 02:42:02 -0500

    Added GetStarted.cshtml.cs

---
+++ Pages/GetStarted.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace isolaatti_API.Pages
+{
+    public class GetStarted : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
