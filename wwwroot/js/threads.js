7626acef2fd834cbfbbd7fa861a24ca139515e2d
erik <erik10cavazos@gmail.com>
2021-06-12 19:48:01 -0500

    new feature: able to record audios for commenting

--- wwwroot/js/threads.js
+++ wwwroot/js/threads.js
@@ -12,6 +12,7 @@
             let vueInstance = new Vue({
                 el: "#vue-container",
                 data: {
+                    userId: userData.id,
                     post: JSON.parse(request.responseText),
                     commentBoxFullSize: false,
                     commentTextarea: "",
@@ -21,12 +22,29 @@
                     audioPlayer: new Audio(),
                     audioUrl: "",
                     playing: false,
-                    paused: false
+                    paused: false,
+                    audio: {
+                        recording: false,
+                        url: "",
+                        timeInSeconds: 0,
+                        recorded: false,
+                        consolidated: false,
+                        recorder: {
+                            mediaRecorder: null,
+                            mediaStream: null,
+                            audioData: [],
+                            audioPlayer: new Audio(),
+                            resultantBlob: null
+                        }
+                    }
                 },
                 computed: {
-                  makeCommentButtonDisabled: function() {
-                      return this.commentTextarea === "" || this.commentTextarea === null
-                  }  
+                    makeCommentButtonDisabled: function() {
+                        return this.commentTextarea === "" || this.commentTextarea === null
+                    },
+                    addAudioButtonText: function() {
+                        return (this.audio.consolidated) ? "Audio recorded" : "Record audio";
+                    }
                 },
                 methods: {
                     likePost: function (post) {
@@ -68,22 +86,45 @@
                         request.send(formData);
                     },
                     makeComment: function () {
-                        console.log("Se hizo el comentario!!");
+                        let globalThis = this;
                         let form = new FormData();
                         form.append("sessionToken", sessionToken);
                         form.append("postId",this.post.id);
                         form.append("content", this.commentTextarea);
                         
-                        let request = new XMLHttpRequest();
-                        request.open("POST","/api/MakeComment");
-                        request.onload = () => {
-                            if(request.status === 200) {
-                                console.log(JSON.parse(request.responseText));
-                                this.commentTextarea = "";
-                                this.getComments();
-                            }
+                        // there is an audio to upload
+                        if(this.audio.consolidated) {
+                            let storageRef = storage.ref();
+                            let date = new Date();
+                            
+                            let uploadTask = storageRef
+                                .child(`audio_comments/${userData.id}/${date.toDateString()}-${date.getHours()}-${date.getMinutes()}-${date.getSeconds()}_audio.webm`);
+                            uploadTask.put(this.audio.recorder.resultantBlob).then(function(snapshot){
+                                uploadTask.getDownloadURL().then(function(url){
+                                    form.append("audioUrl",url);
+                                    sendCommentRequest();
+                                });
+                            });
+                        } else {
+                            sendCommentRequest();
                         }
-                        request.send(form);
+                        
+                        function sendCommentRequest() {
+                            let request = new XMLHttpRequest();
+                            request.open("POST","/api/MakeComment");
+                            request.onload = () => {
+                                if(request.status === 200) {
+                                    console.log("Se hizo el comentario!!");
+                                    console.log(JSON.parse(request.responseText));
+                                    globalThis.commentTextarea = "";
+                                    globalThis.getComments();
+                                    if(globalThis.commentBoxFullSize){
+                                        globalThis.toggleCommentBoxFullSize();
+                                    }
+                                }
+                            }
+                            request.send(form);
+                        }
                     },
                     compileMarkdown: function(raw) {
                         return marked(raw);
@@ -225,6 +266,7 @@
                         request.onload = () => {
                             if(request.status === 200) {
                                 this.comments = JSON.parse(request.responseText);
+                                window.scrollTo(0, document.body.scrollHeight);
                             }
                         }
                         request.send(form);
@@ -248,6 +290,74 @@
                             }
                         }
                         this.playing = true;
+                    },
+                    requestMicrophone: function() {
+                        let audioStreamPromise = navigator.mediaDevices.getUserMedia({audio:true, video:false})
+
+                        audioStreamPromise.then((stream) => {
+                            this.audio.recorder.mediaStream = stream;
+                        });
+                    },
+                    startRecording: function() {
+                        this.audio.consolidated = false;
+                        this.audio.timeInSeconds = 0;
+                        this.audio.recorder.mediaRecorder = new MediaRecorder(this.audio.recorder.mediaStream,{
+                            mimeType: 'audio/webm; codecs=opus',
+                            bitsPerSecond: 128000
+                        });
+                        this.audio.recorder.mediaRecorder.start();
+                        let globalThis = this;
+
+                        // start interval that will be updating the progress bar.
+                        // stop this interval when recording ends
+                        let interval = setInterval(function() {
+                            globalThis.audio.timeInSeconds += 1;
+                        },1000);
+
+                        // This timer is what limits the duration up to 2 minutes (120 seconds)
+                        let timer = setTimeout(function(){
+                            globalThis.stopRecording();
+                            clearInterval(interval);
+                        }, 120*1000);
+
+                        this.audio.recorder.mediaRecorder.ondataavailable = function(e) {
+                            globalThis.audio.recorder.audioData.push(e.data);
+                            console.log("Data pushed...");
+                        }
+                        this.audio.recorder.mediaRecorder.onstop = function() {
+                            clearTimeout(timer);
+                            clearInterval(interval);
+                            globalThis.audio.recorder.resultantBlob =
+                                new Blob(globalThis.audio.recorder.audioData, {'type':'audio/webm; codecs=opus'});
+                        }
+                        this.audio.recording = true;
+                    },
+                    stopRecording: function() {
+                        this.audio.recorder.mediaRecorder.stop();
+                        this.audio.recording = false;
+                        this.audio.recorded = true;
+                    },
+                    playRecord: function() {
+                        this.audio.recorder.audioPlayer.src = window.URL.createObjectURL(this.audio.recorder.resultantBlob);
+                        this.audio.recorder.audioPlayer.volume = 1;
+                        this.audio.recorder.audioPlayer.play().then(function() {
+                            console.log("Playing recorded audio");
+                        }).catch(function(error) {
+                            console.error("Can't play recorded audio: " + error);
+                        });
+                    },
+                    resetRecording: function() {
+                        this.audio.recording = false;
+                        this.audio.recorded = false;
+                        this.audio.recorder.resultantBlob = null;
+                        this.audio.recorder.audioData = [];
+                        this.audio.recorder.audioPlayer.pause();
+                        this.audio.consolidated = false;
+                        this.audio.timeInSeconds = 0;
+                    },
+                    consolidate: function() {
+                        this.audio.consolidated = true;
+                        $('#add-audio-modal').modal('hide');
                     }
                 },
                 mounted: function() {
