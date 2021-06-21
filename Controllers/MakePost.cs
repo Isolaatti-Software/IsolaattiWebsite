3a09690a151d9fb728a99cc6094258d8d47fffb2
erik <erik10cavazos@gmail.com>
2021-06-20 23:17:44 -0500

    modified MakePost controller to also allow editing post

--- Controllers/MakePost.cs
+++ Controllers/MakePost.cs
@@ -25,16 +25,46 @@ namespace isolaatti_API.Controllers
         }
         
         [HttpPost]
-        public IActionResult Index([FromForm] string sessionToken, [FromForm] string password, 
+        public IActionResult Index([FromForm] string sessionToken,
             [FromForm] int privacy = 1, 
             [FromForm] string content = "Well, this post was made without content. Why? Idk",
             [FromForm] string audioUrl = null,
-            [FromForm] string themeJson = null)
+            [FromForm] string themeJson = null,
+            [FromForm] long postId = 0)
         {
             var accountsManager = new Accounts(Db);
             var user = accountsManager.ValidateToken(sessionToken);
             if (user == null) return Unauthorized("Token is not valid");
             
+            // this means user wants to edit existing post
+            if (postId != 0)
+            {
+                var existingPost = Db.SimpleTextPosts.Find(postId);
+                if (existingPost == null) return NotFound("Post not found");
+                if (existingPost.UserId != user.Id) return Unauthorized("Post is not yours, cannot edit");
+                
+                // this means user removed or changed audio, so let's delete from firebase
+                if ((existingPost.AudioAttachedUrl != null && audioUrl == null) || existingPost.AudioAttachedUrl != audioUrl)
+                {
+                    GoogleCloudBucket.GetInstance()
+                        .DeleteFile(Utils.GoogleCloudStorageUrlUtils.GetFileRefFromUrl(existingPost.AudioAttachedUrl));
+                }
+
+                existingPost.Privacy = privacy;
+                existingPost.TextContent = content;
+                existingPost.AudioAttachedUrl = audioUrl;
+                existingPost.ThemeJson = themeJson;
+                
+                Db.SimpleTextPosts.Update(existingPost);
+                Db.SaveChanges();
+                
+                return Ok(new ReturningPostsComposedResponse(existingPost)
+                {
+                    UserName = Db.Users.Find(existingPost.UserId).Name,
+                    Liked = Db.Likes.Any(element => element.PostId == existingPost.Id && element.UserId == user.Id)
+                });
+            }
+
             // Yep, here I can create the post
             var newPost = new SimpleTextPost()
             {
