b60c0534f4de5278fb1c8b83a7cba4d99c97b829
erik <erik10cavazos@gmail.com>
2021-08-17 14:54:33 -0500

    added event handler to realtime-notifications.js and vue.js integration

--- wwwroot/js/realtime-notifications.js
+++ wwwroot/js/realtime-notifications.js
@@ -1,30 +1,99 @@
-const connection = new signalR.HubConnectionBuilder()
-    .withUrl("/notifications_hub").build();
+const vueInstanceForNotifications = new Vue({
+    el: "#vue-container-for-notifications",
+    data: {
+        notificationSliderShowing: false,
+        sessionToken: sessionToken,
+        notifications: []
+    },
+    computed: {
+      unreadCount: function() {
+          return this.notifications.filter((value, index, array) => {
+              return !value.read
+          }).length;
+      }  
+    },
+    methods: {
+        toggleNotificationsSlider: function() {
+            if(this.notificationSliderShowing){
+                this.$refs.notificationsSlider.style.animation = "hideNotificationsContainer";
+                this.$refs.notificationsSlider.style.animationDuration = "0.4s";
+                this.$refs.notificationsSlider.style.animationFillMode = "forwards";
+            } else {
+                this.$refs.notificationsSlider.style.animation = "showNotificationsContainer";
+                this.$refs.notificationsSlider.style.animationDuration = "0.4s";
+                this.$refs.notificationsSlider.style.animationFillMode = "forwards";
 
-async function start() {
-    try {
-        await connection.start();
-        console.log("Conexion exitosa");
+                this.markAsRead();
+            }
+            this.notificationSliderShowing = !this.notificationSliderShowing;
+            
+            
+        },
+        fetchNotifications: function() {
+            let form = new FormData();
+            form.append("sessionToken",this.sessionToken);
+            
+            let request = new XMLHttpRequest();
+            request.open("POST","/api/Notifications/GetAllNotifications");
+            request.onload = () => {
+               if(request.status !== 500){
+                   this.notifications = JSON.parse(request.responseText)
+               }
+            };
+            
+            request.send(form);
+            
+        },
         
-        connection.invoke("EstablishConnection").then(function(data) {
-           console.log(data);
-        }).catch(function (error) {
-            console.error(error);
-        });
+        markAsRead: function() {
+            if(this.unreadCount > 0) {
+                this.notifications.forEach((value => value.read = true));
+                
+                let form = new FormData();
+                form.append("sessionToken", this.sessionToken);
+                
+                let request = new XMLHttpRequest();
+                request.open("POST", "/api/Notifications/MarkAsRead");
+                request.onload = () => {
+                    if(request.status !== 500) {
+                        console.log("all notifications marked as read");
+                    }
+                }
+                request.send(form);
+            }
+        }
+    },
+    mounted: function(){
+        const globalThis = this;
+        const connection = new signalR.HubConnectionBuilder()
+            .withUrl("/notifications_hub").build();
+
+        async function start() {
+            try {
+                await connection.start();
+                console.log("Conexion exitosa");
+                
+                connection.on("SessionSaved", function(serverResponse) {
+                    console.log(serverResponse);
+                });
+
+                connection.on("hola", function(data) {
+                    alert(data);
+                });
+                
+                connection.on("fetchNotification", function(data) {
+                    globalThis.fetchNotifications();
+                });
+            } catch(err) {
+                console.log(err);
+                setTimeout(start, 5000);
+            }
+        }
+
+        connection.onclose(start);
+
+        start();
         
-        connection.on("SessionSaved", function(serverResponse) {
-            console.log(serverResponse);
-        });
-        
-        connection.on("hola", function(data) {
-            alert(data);
-        });
-    } catch(err) {
-        console.log(err);
-        setTimeout(start, 5000);
+        this.fetchNotifications();
     }
-}
-
-connection.onclose(start);
-
-start();
+});
