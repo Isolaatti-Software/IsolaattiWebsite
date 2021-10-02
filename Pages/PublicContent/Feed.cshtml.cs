a65c66b2bb8603d80e9f8f7038e840619f3c3da9
erik <erik10cavazos@gmail.com>
2021-10-01 21:59:40 -0500

    public feed, and some other changes

--- Pages/PublicContent/Feed.cshtml.cs
+++ Pages/PublicContent/Feed.cshtml.cs
@@ -1,12 +1,23 @@
+using System.Collections.Generic;
+using System.Linq;
+using isolaatti_API.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace isolaatti_API.Pages.PublicContent
 {
     public class Feed : PageModel
     {
-        public void OnGet()
+        private readonly DbContextApp _db;
+        public List<SimpleTextPost> FeedContent;
+        public Feed(DbContextApp dbContextApp)
         {
-            
+            _db = dbContextApp;
+        }
+        public IActionResult OnGet()
+        {
+            FeedContent = _db.SimpleTextPosts.Where(post => post.Privacy.Equals(3)).ToList();
+            return Page();
         }
     }
 }
