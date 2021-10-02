fd5afac0cd241ed93a75f22d29c8645c01ed8dec
erik <erik10cavazos@gmail.com>
2021-10-01 21:58:10 -0500

    generate sitemap.txt with all existing public posts

---
+++ Controllers/SitemapGenerator.cs
@@ -0,0 +1,33 @@
+using System.Linq;
+using isolaatti_API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace isolaatti_API.Controllers
+{
+    public class SitemapGenerator : ControllerBase
+    {
+        private readonly DbContextApp _db;
+
+        public SitemapGenerator(DbContextApp dbContextApp)
+        {
+            _db = dbContextApp;
+        }
+        
+        [HttpGet]
+        [Route("/sitemap.txt")]
+        public IActionResult Index()
+        {
+            var response = "https://isolaatti.com/MakeAccount\n";
+            response += "https://isolaatti.com/LogIn\n";
+            response += "https://isolaatti.com/s/Welcome\n";
+            response += "https://isolaatti.com/s/Features\n";
+            response += "https://isolaatti.com/About\n";
+            var publicPosts = _db.SimpleTextPosts.Where(post => post.Privacy.Equals(3)).ToList();
+            foreach (var post in publicPosts)
+            {
+                response += $"https://isolaatti.com/PublicContent/PublicThreadViewer?id={post.Id}\n";
+            }
+            return Ok(response);
+        }
+    }
+}
