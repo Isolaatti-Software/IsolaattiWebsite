770bbc5de19de71d7cffc022e499cf6a12aaf26f
erik <erik10cavazos@gmail.com>
2021-10-10 02:44:05 -0500

    added legal terms

---
+++ Pages/s/Legal/PrivacyPolicy.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace isolaatti_API.Pages.s.Legal
+{
+    public class PrivacyPolicy : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
