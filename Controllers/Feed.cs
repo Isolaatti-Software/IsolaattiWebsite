82155941bc847b821e31e54322a36b7aa9a78044
Erik Cavazos <erik10cavazos@gmail.com>
2021-11-10 22:44:11 -0600

    feed: query improved a little bit

--- Controllers/Feed.cs
+++ Controllers/Feed.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.Json;
+using BenchmarkDotNet.Attributes;
 using isolaatti_API.Classes;
 using isolaatti_API.isolaatti_lib;
 using isolaatti_API.Models;
@@ -20,6 +21,7 @@ namespace isolaatti_API.Controllers
             Db = dbContextApp;
         }
         
+        [Benchmark]
         [HttpPost]
         public IActionResult Index([FromForm] string sessionToken,[FromForm] string postsInDom)
         {
@@ -28,31 +30,46 @@ namespace isolaatti_API.Controllers
             if (user == null) return Unauthorized("Token is not valid");
 
             var followingIds = JsonSerializer.Deserialize<List<Guid>>(user.FollowingIdsJson);
-            var posts = new List<SimpleTextPost>();
+            // var posts = new List<SimpleTextPost>();
             var renderedPosts = JsonSerializer.Deserialize<List<Guid>>(postsInDom);
-            
-            List<UserSeenPostHistory> userPostSeenHistory = Db.UserSeenPostHistories
-                .Where(history => history.UserId.Equals(user.Id)).ToList();
+
+            // var userPostSeenHistory = Db.UserSeenPostHistories
+            //     .Where(history => history.UserId.Equals(user.Id)).AsEnumerable();
 
             // these are all the posts from all the people the user follows and the user itself
-            posts.AddRange(Db.SimpleTextPosts.Where(post => 
-                ((post.Privacy != 1 && followingIds.Contains(post.UserId)) 
-                 || post.UserId.Equals(user.Id)) && !renderedPosts.Contains(post.Id)));
+            // posts.AddRange(Db.SimpleTextPosts
+            //     .Where(post =>
+            //         post.Privacy != 1 &&
+            //         (followingIds.Contains(post.UserId) ||
+            //          post.UserId.Equals(user.Id)) &&
+            //         !renderedPosts.Contains(post.Id)
+            //         && !renderedPosts.Contains(post.Id) &&
+            //         userPostSeenHistory.Where(history => history.PostId.Equals(post.Id)));
 
             
-            foreach (var renderedPostId in renderedPosts)
-            {
-                userPostSeenHistory.RemoveAll(element => element.PostId.Equals(renderedPostId));
-            }
+            // foreach (var renderedPostId in renderedPosts)
+            // {
+            //     userPostSeenHistory.RemoveAll(element => element.PostId.Equals(renderedPostId));
+            // }
 
-            userPostSeenHistory.RemoveAll(element => element.TimesSeen <= 10);
+            var userHistory = Db.UserSeenPostHistories
+                .Where(history => history.UserId.Equals(user.Id) && history.TimesSeen <= 10)
+                .Select(history => history.PostId).ToList();
 
-            foreach (var historySeen in userPostSeenHistory)
-            {
-                posts.RemoveAll(post => post.Id.Equals(historySeen.PostId));
-            }
+            var posts =
+                (from post in Db.SimpleTextPosts
+                    where !userHistory.Contains(post.Id) && followingIds.Contains(post.UserId) &&
+                          !post.Privacy.Equals(1) && !renderedPosts.Contains(post.Id)
+                    orderby post.Date descending
+                    select post).Take(15).ToList();
 
-            posts = posts.OrderByDescending(post => post.Date).Take(15).ToList();
+
+            // foreach (var historySeen in userPostSeenHistory)
+            // {
+            //     posts.RemoveAll(post => post.Id.Equals(historySeen.PostId));
+            // }
+
+            // posts = posts.OrderByDescending(post => post.Date).Take(15).ToList();
 
             var response = new List<ReturningPostsComposedResponse>();
             foreach (var post in posts)
