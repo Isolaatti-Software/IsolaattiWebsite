e9974c9c73c3087309accda608c4afa92e86bb83
Erik Cavazos <erik10cavazos@gmail.com>
2021-12-23 22:12:31 -0600

    translated PasswordRecoveryEmail template into Spanish

--- isolaatti_lib/EmailTemplates.cs
+++ isolaatti_lib/EmailTemplates.cs
@@ -4,11 +4,12 @@
 * This program is not allowed to be copied or reused without explicit permission.
 * erik10cavazos@gmail.com and everardo.cavazoshrnnd@uanl.edu.mx
 */
+
 namespace isolaatti_API.isolaatti_lib
 {
     public class EmailTemplates
     {
-        public const string SongReady= @"<html><body>
+        public const string SongReady = @"<html><body>
         <h1>Song {0} has been processed!</h1>
         <p>Open this link <a href='{5}'>{5}</a></p>
         <p>Thr 4 base tracks are now available on the app. Also, here you have direct links to the files:</p>
@@ -27,8 +28,8 @@ namespace isolaatti_API.isolaatti_lib
         </body></html>";
 
         public const string PasswordRecoveryEmail = @"<html><body>
-            <h1>Hi, let us recover your password!</h1>
-            <p>Just click on the link. You will be asked for a new password. Please do not share this link to anyone.</p>
+            <h1>Hola. Restablezcamos tu contraseña.</h1>
+            <p>Para que puedas restablecer tu contraseña, solo necesitas hacer clic en el enlace. Solo es válido una vez.</p>
             <p><a href='{0}'>{0}</a></p>";
     }
 }
