b33ae3970d98a4d16ed07e8709c0e874c3f94a5c
Erik Cavazos <erik10cavazos@gmail.com>
2021-12-23 22:12:51 -0600

    updated data classes

--- Classes/Profile.cs
+++ Classes/Profile.cs
@@ -4,11 +4,13 @@
 * This program is not allowed to be copied or reused without explicit permission.
 * erik10cavazos@gmail.com and everardo.cavazoshrnnd@uanl.edu.mx
 */
+
 namespace isolaatti_API.Classes
 {
     public class Profile
     {
         public string Username { get; set; }
         public string Email { get; set; }
+        public string Color { get; set; }
     }
 }
