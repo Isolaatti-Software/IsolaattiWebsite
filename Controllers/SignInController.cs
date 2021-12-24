a127cc253d9fb63a422df3388af49b2373e23680
Erik Cavazos <erik10cavazos@gmail.com>
2021-12-23 21:30:05 -0600

    code cleanup

--- Controllers/SignInController.cs
+++ Controllers/SignInController.cs
@@ -7,13 +7,9 @@
 
 //Handles the data to create a new account for users
 
-using System;
-using System.Net.Mail;
 using isolaatti_API.isolaatti_lib;
-using Microsoft.AspNetCore.Mvc;
 using isolaatti_API.Models;
-using MimeKit;
-using SmtpClient = MailKit.Net.Smtp.SmtpClient;
+using Microsoft.AspNetCore.Mvc;
 
 namespace isolaatti_API.Controllers
 {
@@ -22,7 +18,8 @@ namespace isolaatti_API.Controllers
     public class SignIn : ControllerBase
     {
         private readonly DbContextApp dbContextApp;
-        public SignIn (DbContextApp context)
+
+        public SignIn(DbContextApp context)
         {
             dbContextApp = context;
         }
@@ -36,58 +33,11 @@ namespace isolaatti_API.Controllers
          Exception => Returns the exception string if something didn't go well on the server
         */
         [HttpPost]
-        public string Index([FromForm] string username = "", [FromForm] string email = "", [FromForm] string password = "")
+        public string Index([FromForm] string username = "", [FromForm] string email = "",
+            [FromForm] string password = "")
         {
             Accounts accounts = new Accounts(dbContextApp);
             return accounts.MakeAccount(username, email, password);
         }
-        // public bool SendValidationEmail(int id, string validationCode)
-        // {
-        //     string link = String.Format("http://isolaattiapi.azurewebsites.net/validateAccount?id={0}&code={1}",id,validationCode);
-        //     string userEmailAddress = dbContextApp.Users.Find(id).Email;
-        //     string htmlBody = String.Format( @"
-        //         <html>
-        //             <body>
-        //                 <h1>Welcome to Isolaatti</h1>
-        //                 <h1>Bienvenido a Isolaatti</h1>
-        //                 <p>Recently, you used this email address to create a new account on Isolaatti. Click in the link below to activate your account</p>
-        //                 <p>Recientemente, usaste esta dirección de correo electrónico para crear una nueva cuenta en Isolaatti. Haz clic en el link de abajo para activarla</p>
-        //                 <a href='{0}'>{0}</a>
-        //                 <p>If you didn't, please ommit this</p>
-        //                 <p>Si no lo hiciste, omite esto por favor</p>
-        //             </body>
-        //         </html>
-        //         ",link);
-        //
-        //     MimeMessage message = new MimeMessage();
-        //     message.From.Add(new MailboxAddress("Isolaatti","validation.isolaatti@gmail.com"));
-        //     message.To.Add(new MailboxAddress(userEmailAddress));
-        //     message.Subject = "Welcome to Isolaatti | Bienvenido a Isolaatti";
-        //     message.Body = new TextPart(MimeKit.Text.TextFormat.Html)
-        //     {
-        //         Text = htmlBody
-        //     };
-        //
-        //     using (var client = new SmtpClient())
-        //     {
-        //         try
-        //         {
-        //             client.ServerCertificateValidationCallback = (s, c, h, e) => true;
-        //             client.Connect("smtp.gmail.com", 465, true);
-        //             client.Authenticate("validation.isolaatti@gmail.com","0203_0302_" );
-        //             client.Send(message);
-        //             client.Disconnect(true);
-        //         }
-        //         catch (SmtpException exception)
-        //         {
-        //             // this probably means the email address does not exist, so let's delete the account
-        //             var accountToDelete = dbContextApp.Users.Find(id);
-        //             dbContextApp.Users.Remove(accountToDelete);
-        //             dbContextApp.SaveChanges();
-        //         }
-        //         
-        //     }
-        //     return true;
-        // }
     }
 }
