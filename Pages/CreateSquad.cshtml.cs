0c32fe7cfa90375517c57ed1f6243df8999887b2
Erik Cavazos <erik10cavazos@gmail.com>
2022-03-06 23:11:27 -0600

    updated pages

---
+++ Pages/CreateSquad.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace isolaatti_API.Pages
+{
+    public class CreateSquad : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
