68effd7a7fe53b7e6ec7b34e1aad5a2d72c448ae
Erik Cavazos <erik10cavazos@gmail.com>
2022-04-01 01:45:57 -0600

    added key storage db context

---
+++ Models/MyKeysDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.DataProtection.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace isolaatti_API.Models;
+
+public class MyKeysDbContext : DbContext, IDataProtectionKeyContext
+{
+    public MyKeysDbContext(DbContextOptions<MyKeysDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<DataProtectionKey> DataProtectionKeys { get; set; }
+}
