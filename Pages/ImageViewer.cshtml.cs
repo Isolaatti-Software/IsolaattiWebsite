dcaf94cc6c7c164eb9135284d9032818460cf5e8
Erik Cavazos <erik10cavazos@gmail.com>
2022-04-16 19:55:44 -0500

    added profile images list and viewer

---
+++ Pages/ImageViewer.cshtml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading.Tasks;
+using isolaatti_API.isolaatti_lib;
+using isolaatti_API.Models;
+using isolaatti_API.Utils;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace isolaatti_API.Pages;
+
+public class ImageViewer : PageModel
+{
+    private readonly DbContextApp _db;
+
+    public ImageViewer(DbContextApp dbContextApp)
+    {
+        _db = dbContextApp;
+    }
+
+    public ProfileImage Image;
+
+    public async Task<IActionResult> OnGet(Guid imageId)
+    {
+        var token = Request.Cookies["isolaatti_user_session_token"];
+        var accountsManager = new Accounts(_db);
+        var user = await accountsManager.ValidateToken(token);
+        if (user == null)
+        {
+            var protocol = Request.IsHttps ? "https://" : "http://";
+            var url = $"{protocol}{Request.HttpContext.Request.Host.Value}";
+            url += Request.Path;
+            return RedirectToPage("LogIn", new
+            {
+                then = url
+            });
+        }
+
+        // here it's know that account is correct. Data binding!
+        ViewData["name"] = user.Name;
+        ViewData["email"] = user.Email;
+        ViewData["userId"] = user.Id;
+        ViewData["profilePicUrl"] = user.ProfileImageId == null
+            ? null
+            : UrlGenerators.GenerateProfilePictureUrl(user.Id, Request.Cookies["isolaatti_user_session_token"]);
+
+        Image = await _db.ProfileImages.FindAsync(imageId);
+        if (Image == null) return NotFound();
+
+        return Page();
+    }
+}
---
+++ Pages/ProfileImages.cshtml.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using isolaatti_API.isolaatti_lib;
+using isolaatti_API.Models;
+using isolaatti_API.Utils;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace isolaatti_API.Pages;
+
+public class ProfileImages : PageModel
+{
+    private readonly DbContextApp _db;
+
+    public ProfileImages(DbContextApp dbContextApp)
+    {
+        _db = dbContextApp;
+    }
+
+    public List<ProfileImage> ProfileImagesList;
+    public string Name;
+
+    public async Task<IActionResult> OnGet(int id)
+    {
+        var token = Request.Cookies["isolaatti_user_session_token"];
+        var accountsManager = new Accounts(_db);
+        var user = await accountsManager.ValidateToken(token);
+        if (user == null)
+        {
+            var protocol = Request.IsHttps ? "https://" : "http://";
+            var url = $"{protocol}{Request.HttpContext.Request.Host.Value}";
+            url += Request.Path;
+            return RedirectToPage("LogIn", new
+            {
+                then = url
+            });
+        }
+
+        // here it's know that account is correct. Data binding!
+        ViewData["name"] = user.Name;
+        ViewData["email"] = user.Email;
+        ViewData["userId"] = user.Id;
+        ViewData["profilePicUrl"] = user.ProfileImageId == null
+            ? null
+            : UrlGenerators.GenerateProfilePictureUrl(user.Id, Request.Cookies["isolaatti_user_session_token"]);
+
+        var requestedProfile = await _db.Users.FindAsync(id);
+        if (requestedProfile == null) return NotFound();
+        ProfileImagesList = _db.ProfileImages.Where(image => image.UserId == requestedProfile.Id).ToList();
+        Name = requestedProfile.Name;
+        return Page();
+    }
+}
