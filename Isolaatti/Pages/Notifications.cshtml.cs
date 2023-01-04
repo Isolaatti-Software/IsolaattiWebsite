1540e5739a551fcdfae5abc5f088cc4cfac4fb68
Erik Cavazos <erik10cavazos@gmail.com>
2023-01-03 23:33:57 -0600

    added empty page to show notifications

---
+++ Isolaatti/Pages/Notifications.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+using Isolaatti.Services;
+using Isolaatti.Utils;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Isolaatti.Pages;
+
+public class Notifications : PageModel
+{
+    private readonly IAccounts _accounts;
+
+    public Notifications(IAccounts accounts)
+    {
+        _accounts = accounts;
+    }
+    
+    public async Task<IActionResult> OnGet()
+    {
+        var user = await _accounts.ValidateToken(Request.Cookies["isolaatti_user_session_token"]);
+        if (user == null) return RedirectToPage("/LogIn");
+
+        // here it's know that account is correct. Data binding!
+        ViewData["name"] = user.Name;
+        ViewData["email"] = user.Email;
+        ViewData["userId"] = user.Id;
+        ViewData["profilePicUrl"] = user.ProfileImageId == null
+            ? null
+            : UrlGenerators.GenerateProfilePictureUrl(user.Id, Request.Cookies["isolaatti_user_session_token"]);
+
+        return Page();
+    }
+}
