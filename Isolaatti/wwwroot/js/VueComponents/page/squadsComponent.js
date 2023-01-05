ce2c48f9119c7dd8a713d34d752f5480f6748a04
ercavazo <erik.cavazos@elnorte.com>
2023-01-05 14:21:00 -0600

    improved page showing squads on mobile devices

--- Isolaatti/wwwroot/js/VueComponents/page/squadsComponent.js
+++ Isolaatti/wwwroot/js/VueComponents/page/squadsComponent.js
@@ -17,13 +17,13 @@
               <aside class="mt-4 p-2 isolaatti-card">
                 <div class="translucent-white-card">
                   <h3 class="m-2"><i class="fa-solid fa-people-group"></i> Squads</h3>
-                  <div class="d-flex flex-column mt-3">
-                    <button @click="$router.push('/crear')" class="btn btn-sm btn-primary mb-2"><i class="fa-solid fa-plus"></i> Nuevo squad</button>
-                    <div class="dropdown-divider"></div>
-                    <button @click="$router.push('/')" class="btn btn-sm btn-light mb-2">Squads donde eres miembro</button>
-                    <button @click="$router.push('/tuyos')" class="btn btn-sm btn-light mb-2">Tus Squads</button>
-                    <button @click="$router.push('/solicitudes_union')" class="btn btn-sm btn-light mb-2">Solicitudes</button>
-                    <button @click="$router.push('/invitaciones')" class="btn btn-sm btn-light">Invitaciones</button>
+                  <div class="d-flex flex-row flex-lg-column mt-3 flex-wrap justify-content-md-center">
+                    <router-link to="/crear" class="btn" active-class="btn-primary" :exact="true"><i class="fa-solid fa-plus"></i> Nuevo squad</router-link>
+                    <div class="dropdown-divider w-100"></div>
+                    <router-link to="/" class="btn btn-sm" active-class="btn-primary" :exact="true">Squads donde eres miembro</router-link>
+                    <router-link to="/tuyos" class="btn btn-sm" active-class="btn-primary" :exact="true">Tus Squads</router-link>
+                    <router-link to="/solicitudes_union" class="btn btn-sm" active-class="btn-primary" :exact="true">Solicitudes</router-link>
+                    <router-link to="/invitaciones" class="btn btn-sm" active-class="btn-primary" :exact="true">Invitaciones</router-link>
                   </div>
                 </div>
               </aside>
