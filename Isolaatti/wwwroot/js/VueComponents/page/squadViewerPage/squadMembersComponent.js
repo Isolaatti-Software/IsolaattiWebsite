b4155d480393c009a78908638ee5fb4cc6f60f2e
Erik Cavazos <erik10cavazos@gmail.com>
2023-03-05 23:45:18 -0600

    squads: owner, admins and members concept

--- Isolaatti/wwwroot/js/VueComponents/page/squadViewerPage/squadMembersComponent.js
+++ Isolaatti/wwwroot/js/VueComponents/page/squadViewerPage/squadMembersComponent.js
@@ -9,29 +9,79 @@
         return {
             customHeaders: customHttpHeaders,
             members: [],
+            owner: [],
             admins: []
         }
     },
     methods: {
-        fetchMembers: async function() {
-            const response = await fetch(`/api/Squads/${this.squadId}/Members`,{
+        fetchOwner: async function() {
+            const response = await fetch(`/api/Squads/${this.squadId}/Owner`,{
                 method: "GET",
                 headers: this.customHeaders
             });
-            const parsedResponse = await response.json();
-            this.members = parsedResponse.members;
-            this.admins = [parsedResponse.admin];
+            if(response.ok){
+                const parsedResponse = await response.json();
+
+                this.owner = [parsedResponse];
+            }
+            
+        },
+        fetchMembers: async function(refresh) {
+            let url = `/api/Squads/${this.squadId}/Members`;
+            if(refresh) {
+                this.members = [];
+            }
+            if(this.members.length >= 20) {
+                url += `?lastId=${this.members[this.members.length - 1].id}`
+            }
+            const response = await fetch(url,{
+                method: "get",
+                headers: this.customHeaders
+            });
+            
+            if(response.ok){
+                this.members = await response.json();
+            }
+            
+        },
+        fetchAdmins: async function(refresh) {
+            let url = `/api/Squads/${this.squadId}/Admins`;
+            if(refresh) {
+                this.admins = [];
+            }
+            if(this.admins.length >= 20) {
+                url += `?lastId=${this.admins[this.admins.length - 1].id}`
+            }
+            const response = await fetch(url,{
+                method: "get",
+                headers: this.customHeaders
+            });
+
+            if(response.ok){
+                this.admins = await response.json();
+            }
+
         },
         pictureUrl: function(picId) {
             return `/api/Fetch/ProfileImages/${picId}.png`;
         }
     },
     mounted: async function() {
-        await this.fetchMembers();
+        await this.fetchOwner();
+        await this.fetchMembers(true);
+        await this.fetchAdmins(true)
     },
     template: `
     <section class="isolaatti-card">
-      <h5>Administrador</h5>
+      <div class="d-flex w-100">
+        <h5>Propietario</h5>
+        <button class="btn btn-sm btn-link ml-auto">Cambiar</button>
+      </div>
+      <users-grid :users="owner"></users-grid>
+      <div class="d-flex w-100">
+        <h5>Administradores</h5>
+        <button class="btn btn-sm btn-link ml-auto">Agregar</button>
+      </div>
       <users-grid :users="admins"></users-grid>
       <h5>Miembros</h5>
       <users-grid :users="members"></users-grid>
