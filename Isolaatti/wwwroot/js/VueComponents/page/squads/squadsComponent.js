8ac9327a615931f7087ac88d1595f826bd4bcf1e
erik_everardo <erik10cavazos@gmail.com>
2023-06-29 00:15:20 -0600

    agrego modal de crear squad

--- Isolaatti/wwwroot/js/VueComponents/page/squads/squadsComponent.js
+++ Isolaatti/wwwroot/js/VueComponents/page/squads/squadsComponent.js
@@ -12,10 +12,28 @@
     },
     template: `
         <div class="container-fluid">
+        <div class="modal" tabindex="-1" id="createSquad">
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div class="modal-body">
+                <create-squad></create-squad>
+              </div>
+            </div>
+          </div>
+        </div>
+        
         <div class="row">
           <div class="col-12 m-0 jumbotron">
             <h3 class="text-center"><i class="fa-solid fa-people-group"></i> Squads</h3>
             <p class="text-center">Permanece en contacto con las personas que te importan</p>
+            <div class="d-flex justify-content-center">
+                <button class="btn btn-primary" data-toggle="modal" data-target="#createSquad">Nuevo squad</button>
+            </div>
           </div>
         </div>
           <div class="row">
