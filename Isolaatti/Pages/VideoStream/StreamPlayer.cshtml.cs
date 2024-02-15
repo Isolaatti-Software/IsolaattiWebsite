a51b66cd31c78595398177096ad3810c6b0ded6b
erik-everardo <erik10cavazos@gmail.com>
2024-02-14 22:36:07 -0600

    prueba: agrego pagina para ver stream

---
+++ Isolaatti/Pages/VideoStream/StreamPlayer.cshtml.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Threading.Tasks;
+using Isolaatti.AudioStreaming.Entity;
+using Isolaatti.Config;
+using Isolaatti.Models;
+using Isolaatti.Utils;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace Isolaatti.Pages.VideoStream;
+
+public class StreamPlayer : IsolaattiPageModel
+{
+    private readonly DbContextApp _db;
+    private readonly IOptions<Servers> _servers;
+    
+    public RadioStationEntity? StationEntity;
+    public string? DashUrl;
+    public string? HlsUrl;
+
+    public StreamPlayer(DbContextApp db, IOptions<Servers> servers)
+    {
+        _db = db;
+        _servers = servers;
+    }
+
+    public async Task<IActionResult> OnGet([FromQuery] Guid stationId)
+    {
+        var station = await _db.RadioStations.FindAsync(stationId);
+
+        if (station == null)
+        {
+            return NotFound();
+        }
+
+        StationEntity = station;
+
+        DashUrl = _servers.Value.DashUrl?.Replace("[stationId]", stationId.ToString());
+        HlsUrl = _servers.Value.HlsUrl?.Replace("[stationId]", stationId.ToString());
+
+        return Page();
+    }
+}
